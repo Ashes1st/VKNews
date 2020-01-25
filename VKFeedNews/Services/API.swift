a128bd4a0700a86d83e28a579a5ce905e3eb4de4
Ashes1st <atrohov.aleksandr99@gmail.com>
2020-01-25 20:50:02 +0300

    Some network logic. Base for network layer created.

---
+++ VKFeedNews/Services/API.swift
@@ -0,0 +1,17 @@
+//
+//  API.swift
+//  VKFeedNews
+//
+//  Created by Александр on 25.01.2020.
+//  Copyright © 2020 ashes. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.103"
+    
+    static let newsFeed = "/method/newsfeed.get"
+}
